63360b86f280116d06f0cecf11305036ebf2ede1
artificialsoph <s@soph.info>
2019-01-04 10:39:14 -0500

    add init

--- gamma/main.py
+++ gamma/main.py
@@ -1,10 +1,9 @@
 import click
-import pandas as pd
 import yaml
 
-from .utils import get_config
+from .utils import get_config, set_config
 
-pd.set_option('max_colwidth', 200)
+config = get_config()
 
 
 @click.group()
@@ -16,5 +15,19 @@ def gamma():
 @gamma.command()
 def status():
     config = get_config()
-    click.echo("Your current configuration is:")
+    click.secho('Your current configuration is:', bg='magenta', fg='white')
     click.echo(yaml.dump(config, default_flow_style=False))
+
+
+@gamma.command()
+@click.option('-i', "--instructor_repo", type=click.Path(),
+              default=config["instructor_repo"],
+              prompt="Enter the path to the instructor repo")
+@click.option('-s', "--student_repo", type=click.Path(),
+              default=config["student_repo"],
+              prompt="Enter the path to the student repo")
+@click.pass_context
+def init(context, instructor_repo, student_repo):
+    set_config(
+        {"instructor_repo": instructor_repo, "student_repo": student_repo})
+    context.invoke(status)
--- gamma/utils.py
+++ gamma/utils.py
@@ -1,14 +1,28 @@
 from path import Path
 import yaml
 
+CONFIG_PATH = Path("~/.gamma/config.yaml").expanduser()
+
 
 def get_config():
 
-    config_path = Path("~/.gamma/config.yaml").expanduser()
-
-    if not config_path.exists():
-        config_path.parent.makedirs()
-        config_path.write_text(
+    if not CONFIG_PATH.exists():
+        CONFIG_PATH.parent.makedirs()
+        CONFIG_PATH.write_text(
             yaml.dump({"student_repo": "", "instructor_repo": ""}))
 
-    return yaml.load(config_path.text())
+    return yaml.load(CONFIG_PATH.text())
+
+
+def set_config(config_obj):
+
+    # process paths to expanduser
+    for key in ["student_repo", "instructor_repo"]:
+        if "~" in config_obj[key]:
+            config_obj[key] = str(Path(config_obj[key]).expanduser())
+
+    if not CONFIG_PATH.exists():
+        CONFIG_PATH.parent.makedirs()
+    CONFIG_PATH.write_text(yaml.dump(config_obj))
+
+    return True
