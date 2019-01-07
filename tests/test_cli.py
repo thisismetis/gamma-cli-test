a9d5e53bea58caa6b80923cdc9c1ee6e858468c9
artificialsoph <s@soph.info>
2019-01-07 14:04:53 -0500

    add second minimal test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,16 @@
-from gamma import status
+import gamma
 from click.testing import CliRunner
+
+
+def test_gamma():
+
+    runner = CliRunner()
+    result = runner.invoke(gamma)
+    assert result.exit_code == 0
 
 
 def test_status():
 
     runner = CliRunner()
-    result = runner.invoke(status)
+    result = runner.invoke(gamma.status)
     assert result.exit_code == 0
